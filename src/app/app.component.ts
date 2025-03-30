0f5bf0892b642eca2de68dbfd34636346158c491
espedito.melo <espedito.melo@gmail.com>
2025-03-30 08:31:01 -0300

    Angular Material installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  standalone: false,
-  styles: []
+  templateUrl: 'app.component.html',
+  standalone: false
 })
 export class AppComponent {
   title = 'bookstore-front';
